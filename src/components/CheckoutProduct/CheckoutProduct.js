97ce7cb9b2e27d2be78345bf6ab3e2c29b35ecb4
erronintila <erronintila@gmail.com>
2022-07-31 18:42:49 +0800

    Create components for Checkout page

---
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import './CheckoutProduct.css'
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+  const removeFromBasket = () => {
+    console.log('removed')
+  }
+
+  return (
+    <div className='checkoutProduct'>
+      <img src={image} alt='' className='checkoutProduct__image' />
+
+      <div className='checkoutProduct__info'>
+        <p className='checkoutProduct__title'>{title}</p>
+        <p className='checkoutProduct__price'>
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className='checkoutProduct__rating'>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
+      </div>
+    </div>
+  )
+}
+
+export default CheckoutProduct
