5fe4de4e93b3e69f67eabbf9a1964020d6661ee9
erronintila <erronintila@gmail.com>
2022-07-31 18:43:17 +0800

    Create Home page component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header/Header'
+import Home from './pages/Home/Home'
+import Checkout from './pages/Checkout/Checkout'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
